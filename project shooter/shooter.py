5ed7512bff351dfb6fcbe4c347972ab282508f17
bm2000-AH <artemhacumov797@yandex.ru>
2025-02-02 21:59:19 +0300

    StartSreen

--- project shooter/shooter.py
+++ project shooter/shooter.py
@@ -225,13 +225,30 @@ class ShooterGame(pygame.sprite.Sprite):
         return image
 
     def start_screen(self):
-        self.screen.fill(pygame.Color('red'))
+        intro_text = ["                            ShootOGEym", "",
+                      "                 Это игра с повторением заданий экзаменов",
+                      "                 Побеждайте монстров,",
+                      "                 Переходите на локации,",
+                      "                 Открывайте сундуки,",
+                      "                 Выполняйте задания", "",
+                      "                         Нажмите G, чтобы начать игру"]
+        fon = pygame.transform.scale(self.load_image('startscreen.jpg'), (self.width, self.height))
+        self.screen.blit(fon, (0, 0))
+        font = pygame.font.Font(None, 30)
+        text_coord = 50
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color('Brown'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            self.screen.blit(string_rendered, intro_rect)
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.terminate()
-                elif event.type == pygame.KEYDOWN or \
-                        event.type == pygame.MOUSEBUTTONDOWN:
+                elif event.type == pygame.KEYDOWN and event.key == 103:
                     return  # начинаем игру
             pygame.display.flip()
             self.clock.tick(self.fps)
